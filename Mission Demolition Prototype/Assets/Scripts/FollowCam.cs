33ac245ac2b46a0b85d4ec98568de621535259ca
Pinatbutter <dpina@harding.edu>
2020-09-28 10:02:35 -0500

    Uploaded Project (Part 1)

---
+++ Mission Demolition Prototype/Assets/Scripts/FollowCam.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class FollowCam : MonoBehaviour
+{
+    static public GameObject POI;
+    [Header("Set in Inspector")]
+    public float easing = 0.05f;
+    public Vector2 minXY = Vector2.zero;
+
+    [Header("Set Dynamically")]
+    public float camz;
+
+    void Awake()
+    {
+        camz = this.transform.position.z;
+    }
+
+    void FixedUpdate()
+    {
+        if (POI == null) return;
+        Vector3 destination = POI.transform.position;
+        destination.x = Mathf.Max(minXY.x, destination.x);
+        destination.y = Mathf.Max(minXY.y, destination.y);
+        destination = Vector3.Lerp(transform.position, destination, easing);
+        destination.z = camz;
+        transform.position = destination;
+
+        Camera.main.orthographicSize = destination.y + 10;
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
